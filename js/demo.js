a314b31d541df0d44b0faaff104f3ecb5e98032a
Amy <shuierr@126.com>
2018-01-12 17:06:37 +0800

    add demo page logic

--- js/demo.js
+++ js/demo.js
@@ -12,6 +12,7 @@ $(document).ready(function(e) {
     nums.eq(index).siblings().removeClass("numover");
     imgs.eq(index).show();
 
+
     nums.mouseover(function() {
       isStop = true;
       $(this).addClass("numover").siblings().removeClass("numover");
@@ -22,4 +23,49 @@ $(document).ready(function(e) {
       isStop = false;
     });
   }
+
+  $('#rank').click(function() {
+    $('.news').find("li").show();
+  })
+
+  $('#Latest').click(function() {
+    $('.news li').first().hide();
+  })
+
+  $('#recommend').click(function() {
+    $('.news li').last().hide();
+  })
+
+  // 获取图片标签长度
+  count = $("#banner_list img").length;
+  // 不是当前显示的图片隐藏
+  $("#banner_list img:not(:first-child)").hide();
+  // 点击下面的1234按钮，切换图片
+  $("#banner li").click(function() {
+    var i = $(this).text() - 1;
+    n = i;
+    if (i >= count) return;
+    // 淡入淡出效果
+    $("#banner_list img").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
+
+    // 响应点击
+    $(this).toggleClass("on");
+
+    // 切换按钮的时候让上一个按钮回归原来的样式
+    $(this).siblings().removeAttr("class");
+  });
+  t = setInterval("showAuto()", 4000);
+  $("#banner").hover(function() {
+      clearInterval(t)
+    },
+    function() {
+      t = setInterval("showAuto()", 4000);
+    });
+
+  // 自动轮播
+  function showAuto() {
+    n = n >= (count - 1) ? 0 : ++n;
+    $("#banner li").eq(n).trigger('click');
+  }
+
 })
